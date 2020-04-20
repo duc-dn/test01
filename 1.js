4044a218bfa16221396d8529ec67c9193db2161b
duc <ducdo23022001@gmail.com>
2020-04-20 22:56:25 +0700

    upload file hoan thien chua co du lieu

---
+++ 1.js
@@ -0,0 +1,19 @@
+﻿ $(function(){
+     $('.book').click(function (e) { 
+         e.preventDefault();
+         $('.den').addClass('hienlen');
+         $('.menuphai').addClass('ra');
+     });
+     $('#nutclose').click(function (e) { 
+         e.preventDefault();
+         $('.den').removeClass('hienlen');
+         $('.menuphai').removeClass('ra');
+     });
+     $('.hienlen').click(function (e) { 
+         e.preventDefault();
+         $('.den').removeClass('hienlen');
+         $('.menuphai').removeClass('ra');
+     });
+ 
+})  
+ 
